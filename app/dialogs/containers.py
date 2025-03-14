a87c50d46d168d4efb0e8cb50ad8ea0466ef429d
Albert <63642425+iamnalinor@users.noreply.github.com>
2025-03-14 17:30:34 +0300

    Allow uploading homeworks

--- app/dialogs/containers.py
+++ app/dialogs/containers.py
@@ -1,9 +1,11 @@
 import logging
 import operator
+from typing import Any
 
-from aiogram import F, Bot
+from aiogram import F, Bot, types
 from aiogram.types import User
 from aiogram_dialog import Dialog, Window, DialogManager
+from aiogram_dialog.widgets.input import MessageInput
 from aiogram_dialog.widgets.kbd import (
     Next,
     ScrollingGroup,
@@ -14,7 +16,7 @@ from aiogram_dialog.widgets.text import Const, Jinja, Case, Format
 from tortoise.expressions import Q
 
 from app.misc import BACK
-from app.models import Container
+from app.models import Container, Homework
 from app.states import ContainersSG, HomeworksSG
 from app.widgets import Emojize, StartWithSameData
 
@@ -33,24 +35,41 @@ async def open_container(__, ___, manager: DialogManager, container_id: str):
     await manager.start(ContainersSG.view, data={"container_id": int(container_id)})
 
 
-async def container_view_getter(dialog_manager: DialogManager, bot: Bot, **__):
+async def container_view_getter(
+    dialog_manager: DialogManager, bot: Bot, user: User, **__
+) -> dict[str, Any]:
     container_id = dialog_manager.start_data["container_id"]
     container = await Container.get(id=container_id).prefetch_related("owner")
 
-    user = dialog_manager.middleware_data["user"]
     me = await bot.me()
     return {
         "container": container,
         "is_owner": container.owner.id == user.id,
+        "homework_sent": await Homework.get_or_none(container=container, owner=user),
         "invite_link": f"https://t.me/{me.username}?start=cjoin_{container.invite_code}",
     }
 
 
-async def archive_container(__, ___, manager: DialogManager):
+async def archive_container(__, ___, manager: DialogManager) -> None:
     container_id = manager.start_data["container_id"]
     container = await Container.get(id=container_id)
     await container.update_from_dict({"is_archived": True}).save()
     await manager.back()
+
+
+async def homework_handler(message: types.Message, __, manager: DialogManager) -> None:
+    if message.document and message.document.file_size > (5 * 1024 * 1024):
+        await message.answer("Размер файла должен быть не больше 5 МБ.")
+        return
+
+    homework = await Homework.create(
+        owner=manager.middleware_data["user"],
+        container=await Container.get(id=manager.start_data["container_id"]),
+        text=message.text,
+        file_id=message.document.file_id if message.document else None,
+    )
+    await message.answer(f"Отправлено! ID решения — <code>{homework.id}</code>")
+    await manager.done()
 
 
 containers_dialog = Dialog(
@@ -89,7 +108,10 @@ containers_dialog = Dialog(
 {% elif container.deadline %}Дедлайн <b>{{ container.deadline.strftime('%d.%m.%Y') }} 23:59 МСК</b>
 {% endif %}
 
-{% if container.description %}<blockquote expandable>{{ container.description }}</blockquote> 
+{% if container.description %}<blockquote expandable>{{ container.description }}</blockquote>
+
+{% endif %}
+{% if homework_sent %}:white_check_mark: Отправлено решение под ID <code>{{ homework_sent.id }}</code>
 {% endif %}
 """)
         ),
@@ -97,7 +119,7 @@ containers_dialog = Dialog(
             Emojize(":heavy_plus_sign: Загрузить решение"),
             "add_homework",
             state=ContainersSG.add_homework,
-            when=~F["container"].is_archived & ~F["is_owner"],
+            when=~F["container"].is_archived & ~F["is_owner"] & ~F["homework_sent"],
         ),
         StartWithSameData(
             Emojize(":mailbox_with_mail: Решения"),
@@ -146,5 +168,11 @@ containers_dialog = Dialog(
         BACK,
         getter=container_view_getter,
         state=ContainersSG.confirm_archive,
+    ),
+    Window(
+        Emojize(":memo: Отправь решение задания текстом или файлом до 5 МБ."),
+        MessageInput(homework_handler),
+        BACK,
+        state=ContainersSG.add_homework,
     ),
 )
